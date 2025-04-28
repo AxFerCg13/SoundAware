6c79b111639269a97486629447f4eb6ba64a1270
AxFerCg13 <axfervagar@gmail.com>
2025-04-27 20:18:35 -0600

    feat: create AlertAdapter class

---
+++ app/src/main/java/com/example/soundaware/Adapter/AlertAdapter.java
@@ -0,0 +1,58 @@
+package com.example.soundaware.Adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.soundaware.R;
+
+import java.util.List;
+
+public class AlertAdapter extends RecyclerView .Adapter<AlertAdapter.ViewHolder>{
+
+    private List<Alert> alertList;
+    private Context context;
+
+    public AlertAdapter(List<Alert> alertList, Context context) {
+        this.alertList = alertList;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.alert_card, parent);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.alertDate.setText(alertList.get(position).getDate());
+        holder.alertDate.setText(alertList.get(position).getDescription());
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+
+        private ImageView cardIcon;
+        private TextView alertDate;
+        private TextView alertDescription;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            cardIcon = itemView.findViewById(R.id.card_icon);
+            alertDate = itemView.findViewById(R.id.alert_date);
+            alertDescription = itemView.findViewById(R.id.alert_description);
+        }
+    }
+}
