031cb9c6666a0f28a6d8bee0a24e2c3be30f2651
AxFerCg13 <axfervagar@gmail.com>
2025-04-27 19:51:22 -0600

    feat: isert alert class attributes and getters and setters methods

--- app/src/main/java/com/example/soundaware/Adapter/Alert.java
+++ app/src/main/java/com/example/soundaware/Adapter/Alert.java
@@ -1,4 +1,51 @@
 package com.example.soundaware.Adapter;
 
 public class Alert {
+    // Attributes at desing
+    private int id;
+    private String date;
+    private String description;
+
+    public Alert(int id, String date, String description) {
+        this.id = id;
+        this.date = date;
+        this.description = description;
+    }
+
+    public Alert(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "Alert{" +
+                "id=" + id +
+                ", date='" + date + '\'' +
+                ", description='" + description + '\'' +
+                '}';
+    }
 }
