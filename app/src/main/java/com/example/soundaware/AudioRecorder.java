b06ae58cc9fe4b08d8e3442877d9a2292ca1b547
nathalia-dssb <nath.dsbb@gmail.com>
2025-05-03 02:22:42 -0600

    Reapply "added: implemented sound capturing"
    This reverts commit 476f31e40d495b6058163b31147729304c749c55.

---
+++ app/src/main/java/com/example/soundaware/AudioRecorder.java
@@ -0,0 +1,90 @@
+package com.example.soundaware;
+
+import android.media.MediaRecorder;
+import android.util.Log;
+
+
+import java.io.File;
+import java.io.IOException;
+
+
+/***
+ * Clase para manejar la captura de audio
+ */
+public class AudioRecorder {
+    private File RecAudioFile;
+    private MediaRecorder mMediaRecorder;
+    private boolean isRecording = false;
+
+    public AudioRecorder(File file){
+        this.RecAudioFile = file;
+    }
+
+
+    public double getMaxAmplitude() {
+        double amplitude = 0;
+        if (mMediaRecorder != null) {
+            try {
+                amplitude = mMediaRecorder.getMaxAmplitude();
+            } catch (IllegalArgumentException e) {
+                Log.e("MainActivity", "No se pudo obtener amplitud: " + e.getLocalizedMessage());
+            }
+        }
+        return amplitude;
+    }
+
+    public boolean startRecorder(){
+        if (RecAudioFile == null) {
+            return false;
+        }
+        try {
+            mMediaRecorder = new MediaRecorder();
+
+            mMediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+            mMediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+            mMediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+            mMediaRecorder.setOutputFile(RecAudioFile.getAbsolutePath());
+
+            mMediaRecorder.prepare();
+            mMediaRecorder.start();
+            isRecording = true;
+            return isRecording;
+        } catch(IOException e) {
+            mMediaRecorder.reset();
+            mMediaRecorder.release();
+            mMediaRecorder = null;
+            isRecording = false ;
+            Log.e("MainActivity", "Fallo la grabacion: " + e.getLocalizedMessage());
+        }catch(IllegalStateException e){
+            stopRecording();
+            Log.e("MainActivity", "Fallo la grabacion: " + e.getLocalizedMessage());
+            isRecording = false ;
+        }
+        return isRecording;
+    }
+
+    public void stopRecording() {
+        if (mMediaRecorder != null){
+            if(isRecording){
+                try{
+                    mMediaRecorder.stop();
+                    mMediaRecorder.release();
+                }catch(Exception e){
+                    Log.e("MainActivity", "Fallo la grabacion: " + e.getLocalizedMessage());
+                }
+            }
+            mMediaRecorder = null;
+            isRecording = false ;
+        }
+    }
+
+    public void delete() {
+        stopRecording();
+        if (RecAudioFile != null) {
+            RecAudioFile.delete();
+            RecAudioFile = null;
+        }
+    }
+
+
+}
--- app/src/main/java/com/example/soundaware/MainActivity.java
+++ app/src/main/java/com/example/soundaware/MainActivity.java
@@ -1,15 +1,30 @@
 package com.example.soundaware;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
 public class MainActivity extends AppCompatActivity {
+    public static final int REQUEST_AUDIO_PERMISSION_CODE = 200;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +37,56 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
         Toast.makeText(this, "Corriendo en segundo plano", Toast.LENGTH_LONG).show();
+
+        if (!checkPermissions()) requestPermissions();
+
+        //ScheduledThreadPoolExecutor exec = new ScheduledThreadPoolExecutor(1);
+        //exec.scheduleWithFixedDelay(new Runnable() {
+            //public void run() {
+                // codigo a ejecutar repetidas veces
+                if(checkPermissions()) {
+                    File f = new File(getNewFilePath());
+                    AudioRecorder mMediaRecorder = new AudioRecorder(f);
+                    boolean recording = mMediaRecorder.startRecorder();
+
+                    try{
+                        TimeUnit.SECONDS.sleep(10); //Segundos a grabar por cada corte
+                        mMediaRecorder.stopRecording();
+                    }catch(Exception e){
+                       Log.e("MainActivity", "Fallo la grabacion: " + e.getLocalizedMessage());
+                   }
+
+
+                }
+            //}
+        //}, 0, 11, TimeUnit.SECONDS);
+
+
     }
+
+    private String getNewFilePath() {
+        File dir = getExternalFilesDir(null);
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss", Locale.getDefault());
+        String timeStamp = formatter.format(new Date());
+        return new File(dir, "SoundAware_" + timeStamp + ".wav").getAbsolutePath();
+    }
+    private boolean checkPermissions() {
+        return ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) == PackageManager.PERMISSION_GRANTED;
+    }
+    private void requestPermissions() {
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECORD_AUDIO}, REQUEST_AUDIO_PERMISSION_CODE);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_AUDIO_PERMISSION_CODE) {
+            if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                Toast.makeText(this, "Permiso concedido", Toast.LENGTH_LONG).show();
+            }
+        } else {
+            Toast.makeText(this, "Permiso denegado", Toast.LENGTH_LONG).show();
+        }
+    }
+
 }
